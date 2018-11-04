bbdd014b2158d9800712b33bacd88ba6d7abffb6
SilencerWeb <silencer.web@gmail.com>
2018-11-04 07:37:52 +0500

    Add basic project structure

---
+++ src/ui/theme.js
@@ -0,0 +1,19 @@
+const css = String.raw;
+
+
+export const globalStyles = css`
+  *,
+  *:before,
+  *:after {
+    box-sizing: border-box;
+  }
+
+  html {
+    -ms-overflow-style: scrollbar;
+  }
+
+  body {
+    position: relative;
+    min-width: 32rem;
+  }
+`;
